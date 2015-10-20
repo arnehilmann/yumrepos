ed1d03debfa4ef0cf436738ecd001249ff2ca6af
Arne Hilmann <arne@hilmann.de>
2015-10-20 13:15:12 +0200

    test upload to non-existing repo

--- src/tests/scripts/full-lifecycle-tests.sh
+++ src/tests/scripts/full-lifecycle-tests.sh
@@ -5,12 +5,21 @@ TESTREPO=repos/testrepo
 
 rm -rf $TESTREPO
 
-curl http://localhost:8080/repos                -i -s | grep "200 OK"
+set -x
+
+curl http://localhost:8080/repos           -i -s | grep "200 OK"
+
+curl -F rpm=@res/foo-1.0-1.x86_64.rpm \
+    http://localhost:8080/admin/$TESTREPO  -i -s | grep "404 NOT FOUND"
+
 curl -X PUT \
     http://localhost:8080/admin/$TESTREPO  -i -s | grep "201 CREATED"
+
 curl http://localhost:8080/$TESTREPO       -i -s | grep "200 OK"
+
 curl -F rpm=@res/foo-1.0-1.x86_64.rpm \
     http://localhost:8080/admin/$TESTREPO  -i -s | grep "201 CREATED"
+
 curl http://localhost:8080/$TESTREPO       -i -s | grep "200 OK"
 
 [[ -e $TESTREPO/foo-1.0-1.x86_64.rpm ]]
@@ -18,11 +27,17 @@ curl http://localhost:8080/$TESTREPO       -i -s | grep "200 OK"
 
 curl -X DELETE \
     http://localhost:8080/admin/$TESTREPO  -i -s | grep "409 CONFLICT"
+
 curl -X DELETE \
     http://localhost:8080/admin/$TESTREPO/foo-1.0-1.x86_64.rpm -i -s | grep "204 NO CONTENT"
+
 curl -X DELETE \
     http://localhost:8080/admin/$TESTREPO/foo-1.0-1.x86_64.rpm  -i -s | grep "404 NOT FOUND"
+
 curl -X DELETE \
     http://localhost:8080/admin/$TESTREPO  -i -s | grep "204 NO CONTENT"
 
 [[ ! -d $TESTREPO ]]
+
+set +x
+echo "SUCCESS"
