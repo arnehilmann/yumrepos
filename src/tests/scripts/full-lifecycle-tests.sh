b85381ceb025a33053fdf1f3fd44154055a2aa20
Arne Hilmann <arne@hilmann.de>
2015-10-28 09:24:45 +0100

    add link_to test and rename repos to testrepos

--- src/tests/scripts/full-lifecycle-tests.sh
+++ src/tests/scripts/full-lifecycle-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e -E -u -o pipefail -C
 
-TESTREPO1=repos/repo1
-TESTREPO2=repos/repo2
+TESTREPO1=repos/testrepo1
+TESTREPO2=repos/testrepo2
+TESTREPO3=repos/testrepo3
 HOST=http://localhost:8080
 TESTRPM=foo-1.0-1.x86_64.rpm
 
@@ -10,15 +11,16 @@ TEST_YUM=false
 if yum --version &> /dev/null; then
     TEST_YUM=true
     YUM="yum -c res/yum.conf"
-    echo "'yum' command found, testing testrepo now"
+    echo "'yum' command found"
 fi
 
-echo "preparing test repos $TESTREPO1 and $TESTREPO2"
-rm -rf $TESTREPO1 $TESTREPO2
+echo "preparing test repos"
+rm -rf $TESTREPO1 $TESTREPO2 $TESTREPO3
+
 
 # set -x
 
-echo "check repos overview"
+echo "check if yum-repo service is up"
 curl $HOST/repos           -i -s | grep "200 OK"
 
 echo "try to upload rpm to non-existant repo"
@@ -47,45 +49,64 @@ curl -X PUT \
 echo "check metadata"
 [[ -d $TESTREPO2/repodata ]]
 
-if $TEST_YUM; then
-    echo "search for rpm via yum"
-    $YUM clean all
-    $YUM repolist
-    $YUM search foo | grep "foo.x86_64"
-    echo "rpm found in repo: " $($YUM info foo | grep "Repo")
-fi
-
-curl -X STAGE -d target-repo=repo2 $HOST/admin/$TESTREPO1/$TESTRPM
+echo "create empty repo3"
+curl -X PUT \
+    $HOST/admin/$TESTREPO3 -i -s | grep "201 CREATED"
 
 if $TEST_YUM; then
     echo "search for rpm via yum"
     $YUM clean all
     $YUM repolist
     $YUM search foo | grep "foo.x86_64"
-    echo "rpm found in repo: " $($YUM info foo | grep "Repo")
+    echo "rpm found in repo: " $($YUM info foo | grep "testrepo1")
 fi
+
+curl -X STAGE -d target-repo=testrepo2 $HOST/admin/$TESTREPO1/$TESTRPM
+
+if $TEST_YUM; then
+    echo "search for rpm via yum"
+    $YUM clean all
+    $YUM repolist
+    $YUM search foo | grep "foo.x86_64"
+    echo "rpm found in repo: " $($YUM info foo | grep "testrepo2")
+fi
+
+echo "replace repo3 with a link to repo2"
+curl -X DELETE $HOST/admin/$TESTREPO3 -i -s | grep "204 NO CONTENT"
+curl -X PUT $HOST/admin/$TESTREPO3?link_to=testrepo2 -i -s | grep "201 CREATED"
+
+echo "check for repo links"
+curl $HOST/admin/$TESTREPO1/is_link -i -s | grep "false"
+curl $HOST/admin/$TESTREPO3/is_link -i -s | grep "true"
+
+if $TEST_YUM; then
+    echo "search for rpm via yum"
+    $YUM clean all
+    $YUM repolist
+    $YUM search foo | grep "foo.x86_64"
+    echo "rpm found in repo: " $($YUM info foo --showduplicates | grep "repo3")
+fi
+
+echo "tear down test repos"
 
 echo "try to remove non-empty $TESTREPO2"
-curl -X DELETE \
-    $HOST/admin/$TESTREPO2  -i -s | grep "409 CONFLICT"
+curl -X DELETE $HOST/admin/$TESTREPO2  -i -s | grep "409 CONFLICT"
 
 echo "remove rpm"
-curl -X DELETE \
-    $HOST/admin/$TESTREPO2/$TESTRPM -i -s | grep "204 NO CONTENT"
+curl -X DELETE $HOST/admin/$TESTREPO2/$TESTRPM -i -s | grep "204 NO CONTENT"
 
 echo "try to remove rpm that was removed already"
-curl -X DELETE \
-    $HOST/admin/$TESTREPO2/$TESTRPM  -i -s | grep "404 NOT FOUND"
+curl -X DELETE $HOST/admin/$TESTREPO2/$TESTRPM  -i -s | grep "404 NOT FOUND"
 
 echo "remove empty repo $TESTREPO1"
-curl -X DELETE \
-    $HOST/admin/$TESTREPO1  -i -s | grep "204 NO CONTENT"
+curl -X DELETE $HOST/admin/$TESTREPO1  -i -s | grep "204 NO CONTENT"
 
 echo "remove empty repo $TESTREPO2"
-curl -X DELETE \
-    $HOST/admin/$TESTREPO2  -i -s | grep "204 NO CONTENT"
+curl -X DELETE $HOST/admin/$TESTREPO2  -i -s | grep "204 NO CONTENT"
 
 echo "check if repos are removed"
+[[ ! -d $TESTREPO1 ]]
 [[ ! -d $TESTREPO2 ]]
+[[ ! -d $TESTREPO3 ]]
 
 echo "SUCCESS"
