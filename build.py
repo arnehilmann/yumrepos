aeed1b51e84c513c0e73bc31264b984662b891d0
Arne Hilmann <arne@hilmann.de>
2016-04-01 12:42:57 +0200

    fix file permissions in all containers

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ use_plugin('filter_resources')
 name = "yum-repos"
 summary = "yum-repos: simple yum repositories with minimal rest api"
 url = "https://github.com/arnehilmann/yum-repos"
-version = "0.7.9"
+version = "0.7.10"
 
 authors = [Author('Arne Hilmann', 'arne.hilmann@gmail.com')]
 
