b23b76e5fa622c10281aee0fd7ff895d75a05759
Arne Hilmann <arne@hilmann.de>
2016-04-01 11:40:04 +0200

    build rpm for different python versions, in fresh docker container

--- build.py
+++ build.py
@@ -35,5 +35,5 @@ def set_properties(project):
     project.depends_on("flask")
 
     project.set_property('copy_resources_target', '$dir_dist')
-    project.get_property('copy_resources_glob').extend(['setup.cfg'])
-    project.get_property('filter_resources_glob').extend(['**/setup.cfg'])
+    project.get_property('copy_resources_glob').extend(['setup.*cfg'])
+    project.get_property('filter_resources_glob').extend(['**/setup.*cfg'])
