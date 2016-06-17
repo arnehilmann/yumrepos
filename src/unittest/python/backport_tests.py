72fcb82d33c4a4317630b6f2c7985e69ff9d3ce3
Arne Hilmann <arne@hilmann.de>
2016-06-17 15:22:04 +0200

    add some simple tests for lru_cache

---
+++ src/unittest/python/backport_tests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+import unittest
+
+from backports import functools_lru_cache
+
+class Test(unittest.TestCase):
+    def test_with_bound_cache(self):
+        @functools_lru_cache.lru_cache()
+        def cachy(*args):
+            return True
+        self.assertTrue(cachy("foo"))
+        self.assertTrue(cachy("bar"))
+        self.assertTrue(cachy("foo"))
+        print(cachy.cache_info())
+        cachy.cache_clear()
+
+    def test_without_cache(self):
+        @functools_lru_cache.lru_cache(maxsize=None)
+        def cachy(*args):
+            return True
+        self.assertTrue(cachy("foo"))
+        self.assertTrue(cachy("bar"))
+        self.assertTrue(cachy("foo"))
+        print(cachy.cache_info())
+        cachy.cache_clear()
+
+    def test_with_boundless_cache(self):
+        @functools_lru_cache.lru_cache(maxsize=0)
+        def cachy(*args):
+            return True
+        self.assertTrue(cachy("foo"))
+        self.assertTrue(cachy("bar"))
+        self.assertTrue(cachy("foo"))
+        print(cachy.cache_info())
+        cachy.cache_clear()
+
+
+if __name__ == "__main__":
+    sys.exit(unittest.main())
