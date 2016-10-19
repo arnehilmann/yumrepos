b44f8045b9f8a45c0852dc76b4c2eeeba918e09f
Arne Hilmann <arne@hilmann.de>
2016-10-19 09:45:36 +0200

    bump version

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ use_plugin('filter_resources')
 name = "yumrepos"
 summary = "yumrepos: simple yum repositories with minimal rest api"
 url = "https://github.com/arnehilmann/yumrepos"
-version = "0.8.6"
+version = "0.8.7"
 
 authors = [Author('Arne Hilmann', 'arne.hilmann@gmail.com')]
 
